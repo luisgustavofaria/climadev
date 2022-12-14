c73aa9b027f82f939d66d20af3f1fff10c48e20a
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2022-12-13 22:42:14 -0300

    Criado busca de cidades na API

--- src/components/searchCity/SearchCity.jsx
+++ src/components/searchCity/SearchCity.jsx
@@ -1,13 +1,39 @@
+import { useState, useEffect } from "react";
+
 import InputWithSuggestions from "../form/inputWithSuggestions/InputWithSuggestions";
 
 function SearchCity() {
 
+    const [searchCity, setSearchCity] = useState('')
+    const [suggestionCity, setsuggestionCity] = useState([])
     
+    const feachCity = async () => {
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=5&appid=${process.env.NEXT_PUBLIC_OWM_KEY}`)
+        const jsonCity = await response.json() 
+        changeJsonCity(jsonCity);
+    }
+
+    const changeJsonCity = (list) => {
+        const formatted = list.map((obj) => {
+            return {
+                text: `${obj.name}, ${obj.state}, ${obj.country}`,
+                onClick: () => console.log(`LAT: ${obj.lat} LON: ${obj.lon}`)
+            }
+        })
+        setsuggestionCity(formatted)
+    }
+    
+    useEffect(() => {
+        if (searchCity.length > 3) feachCity()
+        
+    }, [searchCity])
 
     return(
         <InputWithSuggestions 
             placeholder='Digite o nome da cidade'
-            
+            value={searchCity}
+            onChange={(event) => {setSearchCity(event.target.value)}}
+            suggestions={suggestionCity}
         />
     )
 }
