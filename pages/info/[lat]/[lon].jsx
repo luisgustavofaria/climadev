c98fd04b41bfe8590eee6faee6515c2f9e6b8f78
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2022-12-19 17:51:35 -0300

    pegando dados do API

--- pages/info/[lat]/[lon].jsx
+++ pages/info/[lat]/[lon].jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import { useState, useEffect } from "react"
+import Router, { useRouter } from "next/router"
 
 import Navbar from "../../../src/components/navbar/Navbar"
 import Container from "../../../src/components/layout/container/Container"
@@ -33,6 +35,32 @@ const InfoContainer = styled.div`
 `
 
 function infoPage() {
+
+    const [currentInfo, setCurrentInfo] = useState()
+    const [futureInfo, setFutureInfo] = useState()
+    
+    const router = useRouter()
+    const {lat, lon} = router.query
+
+    const fetchCurrentInfo = async () => {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OWM_KEY}&units=metric&lang=pt_br`)
+        const json = await response.json()
+        console.log(json);
+    }
+
+    const fetchFutureInfo = async () => {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OWM_KEY}&units=metric&lang=pt_br`)
+        const json = await response.json()
+        console.log(json);
+    }
+
+    useEffect(() => {
+        if (lat && lon) {
+            fetchCurrentInfo()
+            fetchFutureInfo()
+        }
+    }, [lat, lon])
+
     return (
         <>
             <Navbar />
